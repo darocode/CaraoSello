02f3d64348a3fb6864ab9cbee32b8f26db905dad
darocode <derodriguez074@misena.edu.co>
2022-11-01 13:06:29 -0500

    En caso de apostar 0, no lo dejara

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,45 @@
 
-const jugar = document.getElementById('jugar')
+const jugar = document.getElementById('jugar');
 
 var moneda= 0;
 
 var i=0;
 
+
+
 jugar.addEventListener('click',()=>{
 
-    
-
     const apuesta = document.getElementById('apuesta').value;
-    var total=apuesta;
 
-    const animacion = document.getElementById('animacion');
-    animacion.classList.toggle('active')
+    if(apuesta>0){
+        
+            var ciclo = true;
 
-    console.log(apuesta)
+            var total=apuesta;
+
+            const animacion = document.getElementById('animacion');
+            animacion.classList.toggle('active')
+
+            console.log(apuesta)
+
+            while(ciclo==true){
+                i++
+                var maquina= Math.random()*2+1;
+
+                total=parseInt(total);
+                apuesta=parseInt(apuesta);
+
+                moneda=prompt('Elige Cara o Sello \n 1.Cara\n 2.Sello');
+
+                if(moneda==maquina){
+                    total = total+apuesta;
+                    window.alert('Ganasteee')
+                }
+            }
+    }else{
+        window.alert('Tienes que apostar algo mayor a 0')
+    }
+    
 })
 
 
